185186c73bc03d2986e4ef07fafe3ec2f8b1c782
Tung Huynh <19522492@gm.uit.edu.vn>
2021-10-03 19:29:30 +0700

    done ex3

--- ex3-octave/lrCostFunction.m
+++ ex3-octave/lrCostFunction.m
@@ -36,6 +36,11 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X * theta);
+theta(1,1) = 0;
+J = (1/m) * ((-y)' * log(h) - (1-y)' * log(1 -h)) + (lambda/(2*m)) * (theta' * theta);
+grad = (1/m) * X' * (h - y) + (lambda/m) * theta;
+grad(1,1) = (1/m) * X'(1,:) * (h - y);
 
 
 
